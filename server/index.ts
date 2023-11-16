8c707dfe542c4246d0f35bdc6cfd1f3dc1c188c4
Gedeon Lenz <email@gedeonlenz.com>
2023-11-16 14:44:23 -0800

    mysql

--- server/index.ts
+++ server/index.ts
@@ -10,6 +10,13 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
+//heart rate
+//blood oxygen
+//fall detection
+
+//get current heart rate
+//get current blood oxygen
+
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
