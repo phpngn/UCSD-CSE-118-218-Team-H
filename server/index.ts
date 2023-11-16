04ec1a3e4c59685fbe8d0e61d59682a5b84bc5a2
Gedeon Lenz <email@gedeonlenz.com>
2023-11-16 14:56:54 -0800

    added endpoints

--- server/index.ts
+++ server/index.ts
@@ -10,12 +10,33 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
-//heart rate
-//blood oxygen
-//fall detection
+app.post('/data/add/heartrate', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
 
-//get current heart rate
-//get current blood oxygen
+app.post('/data/add/bloodoxygen', (req: Request, res: Response) => {
+    res.send('a');
+});
+
+app.post('/data/add/falldetection', (req: Request, res: Response) => {
+    res.send('ad');
+});
+
+app.post('/data/add/heartrate', (req: Request, res: Response) => {
+    res.send('adr');
+});
+
+app.post('/data/retrieve/heartrate', (req: Request, res: Response) => {
+    res.send('adri');
+});
+
+app.post('/data/retrieve/bloodoxygen', (req: Request, res: Response) => {
+    res.send('adria');
+});
+
+app.post('/data/retrieve/falldetection', (req: Request, res: Response) => {
+    res.send('adrian');
+});
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
