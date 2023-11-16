c201cc688a8eedd4cddcf129bba35a8c9ff84666
Gedeon Lenz <email@gedeonlenz.com>
2023-11-16 14:33:21 -0800

    added package.json

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port:string | undefined = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
