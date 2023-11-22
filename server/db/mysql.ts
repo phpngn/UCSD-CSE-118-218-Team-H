e803ab7d9f8e42651cfaa2ee014c703d1dde24c0
Gedeon Lenz <email@gedeonlenz.com>
2023-11-21 17:06:23 -0800

    mysql functions

--- server/db/mysql.ts
+++ server/db/mysql.ts
@@ -2,13 +2,32 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 import * as fs from "fs";
 
+export type Device = {
+    id?: number,
+    serial: string,
+    name:string
+}
+
+export type Event = {
+    id?: number,
+    device_id: number,
+    type:string,
+    alert:boolean,
+    timestamp:string
+}
+
+export type Datapoint = {
+    id?: number,
+    event_id: number,
+    sensor:string,
+    value:number
+}
+
+
+
+
 dotenv.config();
 
-
-export type QueryResult = {
-    rows: object[],
-    fields: object[]
-}
 export default class DB {
     connection: mysql.Connection | undefined = undefined;
     constructor() {
@@ -23,64 +42,67 @@ export default class DB {
         });
     }
 
+    async executeMultiple(queries: string[], params: any[][] = []) {
+        let resultPromises = [];
+        for (let i = 0; i < queries.length; i++) {
+            resultPromises.push(this.executePreparedStatement(queries[i], params[i]));
+        }
+        return await Promise.all(resultPromises);
+    }
+
+    async executePreparedStatement(sql: string, params: any[] = []) {
+        return await this.connection?.execute(sql, params);
+    }
+
+
+
+
+
+
+
     async initSchema() {
         const schema = fs.readFileSync("./db/schema.sql", 'utf8');
         const statements: string[] = schema.split(";");
         return await this.executeMultiple(statements)
     }
 
-    async executeMultiple(queries: string[], params: any[][] = []) {
-        let resultPromises: Promise<QueryResult>[] = [];
-        for (let i = 0; i < queries.length; i++) {
-            resultPromises.push(this.executePreparedStatement(queries[i], params[i]));
+
+
+
+
+    async insertDevices(device: Device) {
+        let sql = `INSERT INTO Devices (serial, name) VALUES (?,?)`;
+        return await this.executePreparedStatement(sql, [device.serial,device.name]);
+    }
+
+    async insertEvent(device: Event) {
+        let sql = `INSERT INTO Events (device_id, type, alert, timestamp) VALUES (?,?,?,?)`;
+        let result:any =  await this.executePreparedStatement(sql, [device.device_id,device.type,device.alert,device.timestamp]);
+        return result.insertId;
+    }
+
+    async insertDatapoints(datapoints: Datapoint[]) {
+        let entries:{queries:string[],params:any[][]} = {queries:[], params:[]};
+        for(let i = 0; i < datapoints.length; i++) {
+            let datapoint = datapoints[i];
+            let sql = `INSERT INTO Datapoints (event_id, sensor, value) VALUES (?,?,?)`;
+            let params = [datapoint.event_id,datapoint.sensor,datapoint.value]
+            entries.queries.push(sql);
+            entries.params.push(params);
         }
-        Promise.all(resultPromises);
-        return resultPromises;
+        return await this.executeMultiple(entries.queries,entries.params);
     }
 
-    async executePreparedStatement(sql: string, params: any[] = []) {
-        let [rows, fields]: any = await this.connection?.execute(sql, params)
-        let res: QueryResult = { rows: rows, fields: fields };
-        return res;
-    }
 
-    async insertHeartRateData(data: any) {
-        let sql = `INSERT INTO HeartRate (timestamp, value, device_id) VALUES (?,?,?)`;
-        let resultPromises: Promise<QueryResult>[] = [];
-        for (let i = 0; i < data.values.length; i++) {
-            resultPromises.push(this.executePreparedStatement(sql, [data.measurements[i].timestamp, data.measurements[i].value, data.device_id]));
+
+
+    async getDeviceBySerial(serial: string) {
+        let sql = `SELECT * FROM Devices WHERE serial=? LIMIT 1`;
+        let [rows,fields]:any = await this.executePreparedStatement(sql, [serial]);
+        if(rows.length >= 1) {
+            return rows[0] as Device;
         }
-        return await Promise.all(resultPromises);
+        else return undefined;
     }
-
-    async insertBloodOxygenData(data: any) {
-        let sql = `INSERT INTO BloodOxygen (timestamp, value, device_id) VALUES (?,?,?)`;
-        let resultPromises: Promise<QueryResult>[] = [];
-        for (let i = 0; i < data.length; i++) {
-            console.log(data[i])
-            resultPromises.push(this.executePreparedStatement(sql, [data.measurements[i].timestamp, data.measurements[i].value, data.device_id]));
-        }
-        return await Promise.all(resultPromises);
-    }
-
-    async insertFallEventData(data: any) {
-        let sql = `INSERT INTO FallEvents (timestamp, event_type, device_id) VALUES (?,?,?)`;
-        let resultPromises: Promise<QueryResult>[] = [];
-        for (let i = 0; i < data.length; i++) {
-            resultPromises.push(this.executePreparedStatement(sql, [data[i].timestamp, data[i].event_type, data[i].device_id]));
-        }
-        return await Promise.all(resultPromises);
-    }
-
-    async insertNotification(data: any) {
-        let sql = `INSERT INTO notification (timestamp, message, device_id) VALUES (?,?,?)`;
-        let resultPromises: Promise<QueryResult>[] = [];
-        for (let i = 0; i < data.length; i++) {
-            resultPromises.push(this.executePreparedStatement(sql, [data[i].timestamp, data[i].message, data[i].device_id]));
-        }
-        return await Promise.all(resultPromises);
-    }
-
-
 
 }
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,37 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import DB from './db/mysql';
+import DB, {Datapoint, Device, QueryResult} from './db/mysql';
+import {type Event} from "./db/mysql";
+
+export enum EventType {
+    Heartrate = "heartrate",
+    Bloodoxygen = "bloodoxygen",
+    Fall = "fall"
+}
+
+export enum EventLevel {
+    Report="report",
+    Alert="alert"
+}
+
+export enum Sensor {
+    Heartrate = "heartrate",
+    Bloodoxygen = "bloodoxygen",
+    Fall = "fall"
+}
+
+export type RequestDatapoint = {
+    sensor:Sensor,
+    value: number
+}
+
+export type EventRequest = {
+    event_type: EventType,
+    event_level: EventLevel,
+    device: string,
+    timestamp:string,
+    datapoints: RequestDatapoint[]
+}
 
 dotenv.config();
 
@@ -20,22 +51,37 @@ app.get('/api/schema', async (req: Request, res: Response) => {
     res.send('Ok');
 });
 
-app.post('/api/event', (req: Request, res: Response) => {
-    switch(req.body.sensor) {
-        case 'heartrate':
-            db.insertHeartRateData(req.body);
-            break;
-        case 'bloodoxygen':
-            db.insertBloodOxygenData(req.body);
-            break;
-        case 'falldetection':
-            db.insertFallEventData(req.body);
-            break;
-        default:
-            console.log("Unknown sensor type");
-            res.send('Unknown sensor type');
+app.post('/api/event', async (req: Request, res: Response) => {
+
+    let requestObj:EventRequest = req.body;
+
+    let device:Device | undefined = await db.getDeviceBySerial(requestObj.device);
+
+    if(device === undefined || device.id === undefined) {
+        res.status(401).send("Error");
+        return;
     }
-    
+
+    let newEvent:Event = {
+        device_id: device.id,
+        type: requestObj.event_type,
+        alert: requestObj.event_level === EventLevel.Alert,
+        timestamp: requestObj.timestamp
+    };
+    let insertEventResult = await db.insertEvent(newEvent);
+
+    let dps:Datapoint[] = [];
+    for(let i = 0; i < requestObj.datapoints.length; i++) {
+        let datapoint:RequestDatapoint = requestObj.datapoints[i];
+        let newDatapoint:Datapoint = {
+            event_id: event_id,
+            sensor: datapoint.sensor,
+            value: datapoint.value
+        }
+        dps.push(newDatapoint);
+    }
+    let insertDPsResult = await db.insertDatapoints(dps);
+
     res.send('Ok');
 });
 
