7a2156d98c1d04708a8b46eb56c18ab4c1b9f4a5
allesklardy <adrian@tum.social>
2023-11-21 18:49:09 -0800

    wird schon

--- server/db/mysql.ts
+++ server/db/mysql.ts
@@ -5,22 +5,22 @@ import * as fs from "fs";
 export type Device = {
     id?: number,
     serial: string,
-    name:string
+    name: string
 }
 
 export type Event = {
     id?: number,
     device_id: number,
-    type:string,
-    alert:boolean,
-    timestamp:string
+    type: string,
+    alert: boolean,
+    timestamp: string
 }
 
 export type Datapoint = {
     id?: number,
     event_id: number,
-    sensor:string,
-    value:number
+    sensor: string,
+    value: number
 }
 
 
@@ -55,11 +55,6 @@ export default class DB {
     }
 
 
-
-
-
-
-
     async initSchema() {
         const schema = fs.readFileSync("./db/schema.sql", 'utf8');
         const statements: string[] = schema.split(";");
@@ -67,45 +62,37 @@ export default class DB {
     }
 
 
-
-
-
     async insertDevices(device: Device) {
         let sql = `INSERT INTO Devices (serial, name) VALUES (?,?)`;
-        return await this.executePreparedStatement(sql, [device.serial,device.name]);
-        //todo extract deviceid
+        let result: any = await this.executePreparedStatement(sql, [device.serial, device.name]);
+        return result.insertId;
     }
 
-    async insertEvent(device: Event) {
+    async insertEvent(device: Event) : Promise<number> {
         let sql = `INSERT INTO Events (device_id, type, alert, timestamp) VALUES (?,?,?,?)`;
-        let result:any =  await this.executePreparedStatement(sql, [device.device_id,device.type,device.alert,device.timestamp]);
-        return result.insertId;
-        //todo extract eventid
+        let result: any = await this.executePreparedStatement(sql, [device.device_id, device.type, device.alert, device.timestamp]);
+        console.log(result)
+        return result[0].insertId;
     }
 
     async insertDatapoints(datapoints: Datapoint[]) {
-        let entries:{queries:string[],params:any[][]} = {queries:[], params:[]};
-        for(let i = 0; i < datapoints.length; i++) {
+        let entries: { queries: string[], params: any[][] } = { queries: [], params: [] };
+        for (let i = 0; i < datapoints.length; i++) {
             let datapoint = datapoints[i];
             let sql = `INSERT INTO Datapoints (event_id, sensor, value) VALUES (?,?,?)`;
-            let params = [datapoint.event_id,datapoint.sensor,datapoint.value]
+            let params = [datapoint.event_id, datapoint.sensor, datapoint.value]
             entries.queries.push(sql);
             entries.params.push(params);
         }
-        return await this.executeMultiple(entries.queries,entries.params);
-        //todo extract inserted datapoint entries/ids
+        return await this.executeMultiple(entries.queries, entries.params);
     }
-
-
-
 
     async getDeviceBySerial(serial: string) {
         let sql = `SELECT * FROM Devices WHERE serial=? LIMIT 1`;
-        let [rows,fields]:any = await this.executePreparedStatement(sql, [serial]);
-        if(rows.length >= 1) {
+        let [rows, fields]: any = await this.executePreparedStatement(sql, [serial]);
+        if (rows.length >= 1) {
             return rows[0] as Device;
         }
         else return undefined;
     }
-
 }
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import DB, {Datapoint, Device, QueryResult} from './db/mysql';
-import {type Event} from "./db/mysql";
+import DB, { Datapoint, Device } from './db/mysql';
+import { type Event } from "./db/mysql";
 
 export enum EventType {
     Heartrate = "heartrate",
@@ -10,8 +10,8 @@ export enum EventType {
 }
 
 export enum EventLevel {
-    Report="report",
-    Alert="alert"
+    Report = "report",
+    Alert = "alert"
 }
 
 export enum Sensor {
@@ -21,7 +21,7 @@ export enum Sensor {
 }
 
 export type RequestDatapoint = {
-    sensor:Sensor,
+    sensor: Sensor,
     value: number
 }
 
@@ -29,7 +29,7 @@ export type EventRequest = {
     event_type: EventType,
     event_level: EventLevel,
     device: string,
-    timestamp:string,
+    timestamp: string,
     datapoints: RequestDatapoint[]
 }
 
@@ -37,11 +37,11 @@ dotenv.config();
 
 const app: Express = express();
 app.use(express.json());
-const port:string | undefined = process.env.PORT || '3000';
+const port: string | undefined = process.env.PORT || '3000';
 const db = new DB();
 
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
     res.send('Ubiqui.care API');
 });
 
@@ -51,38 +51,46 @@ app.get('/api/schema', async (req: Request, res: Response) => {
     res.send('Ok');
 });
 
-app.post('/api/event', async (req: Request, res: Response) => {
+const insertEventHandler = async (requestObj: EventRequest) => {
+    let device: Device | undefined = await db.getDeviceBySerial(requestObj.device);
 
-    let requestObj:EventRequest = req.body;
-
-    let device:Device | undefined = await db.getDeviceBySerial(requestObj.device);
-
-    if(device === undefined || device.id === undefined) {
-        res.status(401).send("Error");
-        return;
+    if (device === undefined || device.id === undefined) {
+        throw new Error("Device not found");
     }
 
-    let newEvent:Event = {
+    let newEvent: Event = {
         device_id: device.id,
         type: requestObj.event_type,
         alert: requestObj.event_level === EventLevel.Alert,
         timestamp: requestObj.timestamp
     };
-    let insertEventResult = await db.insertEvent(newEvent);
-    //todo extract eventid
-    let dps:Datapoint[] = [];
-    for(let i = 0; i < requestObj.datapoints.length; i++) {
-        let datapoint:RequestDatapoint = requestObj.datapoints[i];
-        let newDatapoint:Datapoint = {
+
+    console.log(newEvent);
+    let event_id = await db.insertEvent(newEvent);
+    console.log("Inserted event with id: " + event_id)
+
+    let dps: Datapoint[] = [];
+    for (let i = 0; i < requestObj.datapoints.length; i++) {
+        let datapoint: RequestDatapoint = requestObj.datapoints[i];
+        let newDatapoint: Datapoint = {
             event_id: event_id,
             sensor: datapoint.sensor,
             value: datapoint.value
         }
         dps.push(newDatapoint);
     }
+    //TODO check if insert successful otherwise return error
     let insertDPsResult = await db.insertDatapoints(dps);
-    //todo check if insert successful otherwise return error
-    res.send('Ok');
+}
+
+app.post('/api/event', async (req: Request, res: Response) => {
+    let promises = [];
+    for (let i = 0; i < req.body.length; i++) {
+        let requestObj = req.body[i];
+        promises.push(insertEventHandler(requestObj));
+    }
+    await Promise.all(promises);
+    res.send('{"message": "ok"}');
 });
 
 app.listen(port, () => {
