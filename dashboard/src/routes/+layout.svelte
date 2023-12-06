137ad6ae068523e52b0fe67d56ccb5f3cdece9d2
allesklardy <adrian@tum.social>
2023-12-05 16:56:41 -0800

    init sveltekit

---
+++ dashboard/src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+  import {
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+  } from "flowbite-svelte";
+  import "../app.pcss";
+
+</script>
+
+<div class="container h-screen w-screen mx-auto">
+  <Navbar class="text-center">
+    <NavBrand href="/">
+      <span
+        class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+        >Ubiqui.Care</span
+      >
+    </NavBrand>
+    <NavHamburger />
+    <NavUl>
+      <NavLi href="/">Home</NavLi>
+      <NavLi href="/docs/components/navbar">Navbar</NavLi>
+      <NavLi href="/pricing">Pricing</NavLi>
+    </NavUl>
+  </Navbar>
+  <slot />
+</div>
---
+++ dashboard/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {Card} from "flowbite-svelte";
+    import { Heading, P, A, Mark, Secondary } from 'flowbite-svelte';
+    let heartrate = 0;
+
+    setInterval(() => {
+        heartrate = Math.floor(Math.random() * 100);
+    }, 1000);
+</script>
+
+<div class="text-center">
+  <Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">Ququi Care</Heading>
+  <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"></P>
+</div>
+
+<Card class="text-5xl font-bold text-center py-15">
+    {heartrate}
+</Card>
