c1d467137c9292e69f912b3715c2dac6b2bba31b
Gedeon Lenz <email@gedeonlenz.com>
2023-12-09 20:10:29 -0800

    new dashboard

--- dashboard/UbiquiCareDashboard/src/lib/data.ts
+++ dashboard/UbiquiCareDashboard/src/lib/data.ts
@@ -4,13 +4,27 @@ import {type Writable, writable} from 'svelte/store';
 // Initial value of the heart rate store
 export const currentHeartRate:Writable<Number> = writable(-1);
 export const isFall:Writable<Boolean> = writable(false);
+export const isEmergency:Writable<Boolean> = writable(false);
+
+export type Notification = {
+    id: number,
+    device_id: number,
+    title: string,
+    checked: boolean,
+    timestamp: string,
+}
+export const notifications:Writable<Notification[]> = writable([]);
 
 const executeSecondsHeartRate = 1;
 const executeSecondsIsFall = 1;
+const executeSecondsIsEmergency = 1;
+const executeSecondsNotifications = 1;
 
 export function startAllSchedulers() {
     startHeartRateScheduler();
     startIsFallScheduler();
+    startEmergencyScheduler();
+    startNotificationsScheduler();
 }
 
 export function startHeartRateScheduler() {
@@ -27,8 +41,23 @@ export function startIsFallScheduler() {
     }, executeSecondsIsFall * 1000);
 }
 
+export function startEmergencyScheduler() {
+    fetchCurrentIsEmergency();
+    return setInterval(async () => {
+        await fetchCurrentIsEmergency();
+    }, executeSecondsIsEmergency * 1000);
+}
 
-const backendHost = 'https://ubiqui-care.fly.dev';
+export function startNotificationsScheduler() {
+    fetchCurrentNotifications();
+    return setInterval(async () => {
+        await fetchCurrentNotifications();
+    }, executeSecondsNotifications * 1000);
+}
+
+
+//const backendHost = 'https://ubiqui-care.fly.dev';
+const backendHost = 'http://localhost:8000';
 
 export async function fetchCurrentHeartRate() {
     try {
@@ -60,4 +89,33 @@ export async function fetchCurrentIsFall() {
         return;
     } catch (error) {}
     isFall.set(false);
+}
+
+export async function fetchCurrentIsEmergency() {
+    try {
+        const response = await fetch(backendHost+'/api/emergency');
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        const data = await response.json();
+        isEmergency.set(data.message === 'ok');
+        return;
+    } catch (error) {}
+    isEmergency.set(false);
+}
+
+export async function fetchCurrentNotifications() {
+    try {
+        const response = await fetch(backendHost+'/api/notifications/all');
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        const data = await response.json();
+        notifications.set(data);
+        return;
+    } catch (error) {}
+    isEmergency.set(false);
 }
--- dashboard/UbiquiCareDashboard/src/routes/+page.svelte
+++ dashboard/UbiquiCareDashboard/src/routes/+page.svelte
@@ -4,61 +4,195 @@
 </svelte:head>
 <script>
 	import { onMount } from 'svelte';
-	import {currentHeartRate, isFall, startAllSchedulers} from "$lib/data";
+	import {currentHeartRate, isEmergency, isFall, notifications, startAllSchedulers} from "$lib/data";
 	onMount(startAllSchedulers);
+
+	let mockNotify = [
+		{
+			id:1,
+			device_id:1,
+			title: 'Heart rate is too high',
+			timestamp: '2021-05-01 12:00:00'
+		},
+		{
+			id:2,
+			device_id:1,
+			title: 'Heart rate is too high',
+			timestamp: '2021-05-01 12:00:00'
+		},
+		{
+			id:3,
+			device_id:1,
+			title: 'Heart rate is too high',
+			timestamp: '2021-05-01 12:00:00'
+		}
+	]
 </script>
 <style>
 
-	.hr span, .fall span {
+	* {
+		font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+		font-size: 14px;
+		font-weight: 400;
+		color: #3b3b3b;
+	}
+
+	h1 {
 		display: block;
 		position: relative;
 		width: 100%;
-		line-height: 100vh;
-		text-align: center;
-		font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-		font-size: 50px;
+		height: 50px;
 		font-weight: 600;
+		font-size: 25px;
+		margin-left: 30px;
+		margin-top: 50px;
+		margin-bottom: 0;;
 	}
 
-	.hr {
+	.container {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: auto;
+		padding: 20px;
+	}
+
+	.widget {
+		display: inline-block;
+		position: relative;
+		vertical-align: top;
+		width: 300px;
+		height: 250px;
+		border-radius: 6px;
+		box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+		margin: 0 10px
+	}
+
+	#heartrate .value {
 		display: block;
 		position: absolute;
-		width: 50%;
-		height: 100%;
-		left:0;
+		width: 100%;
+		font-size: 65px;
+		font-weight: 500;
+		text-align: center;
+		top:50%;
+		transform: translateY(-50%);
+		color: #ffffff;
+		margin-top: -15px;
 	}
 
-	.fall {
+	#heartrate .label {
 		display: block;
 		position: absolute;
-		width: 50%;
-		height: 100%;
-		right:0;
-	}
-	.fallen {
-		background: #ff0000;
+		width: 100%;
+		font-size: 16px;
+		font-weight: 500;
+		text-align: center;
+		top:50%;
+		transform: translateY(-50%);
+		margin-top: 35px;
 		color: #ffffff;
 	}
-
-	.standing {
-		background: #80ea2e;
+	#falls img {
+		display: block;
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+		width: 80px;
+		height: 80px;
+	}
+	#emergency img {
+		display: block;
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+		width: 60px;
+		height: 60px;
+		margin-top: -15px;
+	}
+	#emergency .label {
+		display: block;
+		position: absolute;
+		width: 100%;
+		font-size: 16px;
+		font-weight: 500;
+		text-align: center;
+		top:50%;
+		transform: translateY(-50%);
+		margin-top: 35px;
 		color: #ffffff;
 	}
-
-	.unknown {
-		background: #c2c2c2;
+	#notifications {
+		width: 600px;
+		min-height: 244px;
+		height: auto;
+		max-height: 487px;
+		background: #fcfcfc;
+		overflow-x: hidden;
+		overflow-y: auto;
+	}
+	.notification {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 60px;
+		border-bottom: 1px solid rgba(0,0,0,0.1);
+	}
+	.notification img {
+		display: inline-block;
+		position: relative;
+		vertical-align: middle;
+		width: 30px;
+		height: 30px;
+		margin-left: 20px;
+		margin-right: 25px;
+	}
+	.notification .title {
+		display: inline-block;
+		position: relative;
+		vertical-align: middle;
+		font-size: 16px;
+		font-weight: 500;
 		color: #3b3b3b;
+		line-height: 60px;
+		max-width: 360px;
+		overflow: hidden;
+		white-space: nowrap;
 	}
-	.loaded {
-		background: #252525;
-		color: #ffffff;
+	.notification .timestamp {
+		display: inline-block;
+		position: absolute;
+		right: 20px;
+		top: 50%;
+		transform: translateY(-50%);
+		font-size: 14px;
+		font-weight: 400;
+		color: #afafaf;
+		line-height: 60px;
 	}
 </style>
+<h1>Dashboard</h1>
 <div class="container">
-	<div class="hr {$currentHeartRate == -1 ? 'unknown' : 'loaded'}">
-		<span class="heartrate">Current heart rate: {$currentHeartRate != -1 ? $currentHeartRate : 'Unavailable'}</span>
+	<div class="widget" id="heartrate" style="background: {$currentHeartRate > 50 ? ($currentHeartRate > 100 ? $currentHeartRate > 200 ? '#ff0000' : '#ff7200' : '#4db724') : '#ff0000'}">
+		<span class="value">{$currentHeartRate !== -1 ? $currentHeartRate : '?'}</span>
+		<span class="label">Heart rate</span>
 	</div>
-	<div class="fall {$isFall ? 'fallen' : 'standing'}">
-		<span class="fall">{$isFall ? 'Fallen' : 'Standing'}</span>
+	<div class="widget" id="falls" style="background: {$isFall ? '#ff0000' : '#4db724'}">
+		<img src="{$isFall ? '/falling.png' : '/standing.png'}" alt="" class="value">
+	</div>
+	<div class="widget" id="emergency" style="background: {$isEmergency ? '#ff0000' : '#4db724'}">
+		<img src="{$isEmergency ? '/sos.png' : '/ok.png'}" alt="" class="value">
+		<span class="label">Emergency</span>
+	</div>
+	<div class="widget" id="notifications">
+		{#each mockNotify as notification}
+			<div class="notification">
+				<img src="/notification.png" alt="" class="icon">
+				<span class="title">{notification.title}</span>
+				<span class="timestamp">{notification.timestamp}</span>
+			</div>
+		{/each}
 	</div>
 </div>
