e55bf337a68f19252945910d844fe9fedc84cfa3
Bjarne Sauer <bjarne.sauer@tum.de>
2023-12-11 10:29:17 -0800

    Rename event_type to "fall"

--- sensors/watch-sensors/app/src/main/java/edu/ucsd/cse118/ubiquicare/model/HealthDataReport.java
+++ sensors/watch-sensors/app/src/main/java/edu/ucsd/cse118/ubiquicare/model/HealthDataReport.java
@@ -10,7 +10,7 @@ public class HealthDataReport {
         HEART_RATE,
         @SerializedName("bloodoxygen")
         BLOOD_OXYGEN,
-        @SerializedName("falldetection")
+        @SerializedName("fall")
         FALL_DETECTION;
     }
 
